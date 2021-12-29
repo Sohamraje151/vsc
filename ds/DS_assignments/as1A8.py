9699c4081f23349e7b10b903999e34702f20c3da
Sohamraje151 <soham@s.com>
2021-12-29 23:19:05 +0530

    datascience assiginment 1 set A

---
+++ ds/DS_assignments/as1A8.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+data=[['soham',19,92],['suhas',20,93],['sanket',21,89],['sagar',20,91],['sahil',25,90],['sidhant',22,87],['sudhir',24,67],['shailesh',21,85],['sumit',20,78],['shubham',23,74]]
+df=pd.DataFrame(data,columns=['name','age','percentage'])
+df.plot.scatter(x="name",y="percentage")
+plt.show()
